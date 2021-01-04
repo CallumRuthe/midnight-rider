370774090a9b1455d0e8a64f08e79f6544023fb7
Callum Ruthe <CR000052@sd38.bc.ca>
2021-01-04 11:32:17 -0800

    Revert "added the status check choice to main.py"
    This reverts commit 13c0f527

--- main.py
+++ main.py
@@ -26,7 +26,6 @@ REACH THE END BEFORE THE MAN GON GETCHU.
 
 CHOICES = """
     ----
-    E. Status Check
     Q. QUIT
     ----
 """
@@ -42,17 +41,9 @@ def intro():
 
 
 def main():
-    # intro()
+    intro()
 
-    # Variables
     done = False
-
-    km_traveled = 0             # 100km traveled is the goal
-    agents_distance = -20.0
-    turns = 0                   # amount of turns taken
-    tofu = 3                    # 3 is max tofu
-    fuel = 50                   # 50 is a full tank
-    hunger = 0                  # hunger increases with num
 
     while not done:
         # TODO:  Check if reached END GAME
@@ -62,19 +53,8 @@ def main():
 
         # Handle user's input
         users_choice = input("What do you want to do? ").lower().strip("!,.? ")
-
-        if users_choice == "e":
-            print(f"\t---Status Check---")
-            print(f"\tkm traveled: {km_traveled} km")
-            print(f"\tFuel left: {fuel} L")
-            print(f"\tAgents are {abs(agents_distance)} kms behind you.")
-            print(f"\tYou have {tofu} tofu left.")
-            print(f"\t------------------\n")
-        elif users_choice == "q":
+        if users_choice == "q":
             done = True
-
-        # Pause
-        time.sleep(2)
 
         # TODO: Change the environment based on choice and RNG
 
